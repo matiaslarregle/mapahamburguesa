fb0309e35b7fb7ea814440f4ab7ecc67fb847c71
Matias Larregle <160199344+matiaslarregle@users.noreply.github.com>
2026-06-18 02:17:25 -0300

    Update auth.js

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -14,7 +14,7 @@
           await window.supabaseClient.auth.signInWithOAuth({
             provider: "google",
             options: {
-              redirectTo: window.location.origin,
+              redirectTo: `${window.location.origin}/index.html`
               queryParams: { access_type: "offline", prompt: "consent" },
             },
           });
